78670d29289ab740f77a298a1b45acf501fac965
slordian <arthur.silva.2@academico.ifpb.edu.br>
2023-03-27 10:52:46 -0300

    de novo

--- express/hello-api/index.js
+++ express/hello-api/index.js
@@ -9,6 +9,20 @@ server.get('/', (req, res) => {
   res.send('Viva a Anarquiaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
 });
 
+server.get('/hello/en', (req, res) => {
+  res.send('hello garbage')
+});
+
+server.get('/hello/pt', (req, res) => {
+  res.send('oi imundo')
+});
+
+server.get('/teste/pt', (req, res) => {
+  const nome = req.query.nome; 
+  const idade =  req.query.idade;
+  res.send(`${nome}, ${idade}`);
+}); 
+
 server.listen(3000, () => {
   console.log('O servidor está rodando')
 });
