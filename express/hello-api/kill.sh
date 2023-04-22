6cf33d4afb862e621a68603d1bf6b3c129558b5e
Arthur Felipe Araújo da Silva <arthur@artu.joselito.com>
2023-04-22 12:27:52 -0300

    yarn adicionado

--- express/hello-api/kill.sh
+++ express/hello-api/kill.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # forçar o fechamento do servidor através do kill
-lsof -i :8080 > retorno
+retorno=$(lsof -i :8080)
 
-RETORNO=$(grep -E "node" retorno)
+RETORNO=$(grep -E "node" $retorno)
 KILL=${RETORNO:5:9}
 kill $KILL
