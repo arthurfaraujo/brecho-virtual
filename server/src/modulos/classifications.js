9b93cc3f49338e544b80633c4cdb78695ab7160c
slordian <arthurfelipe5567@gmail.com>
2023-05-11 16:57:01 -0300

    aee ta funcionando o classifications

--- server/src/modulos/classifications.js
+++ server/src/modulos/classifications.js
@@ -1,42 +1,42 @@
-import database from '../database/database.js';
+import database from "../database/database.js";
 
 export async function create(classif) {
-    const db = await database.connect();
+  const db = await database.connect();
 
-    const request = `insert into classificacao values (?, ?, ?)`;
+  const request = `insert into classificacao values (?, ?, ?)`;
 
-    const { lastID } = await db.run(request, classif);
+  const { lastID } = await db.run(request, classif);
 
-    return lastID;
+  return lastID;
 }
 
 export async function readAll() {
-    const db = await database.connect();
+  const db = await database.connect();
 
-    const request = `select * from classificacao;`;
+  const request = `select * from classificacao;`;
 
-    const classificacoes = await db.all(request);
+  const classificacoes = await db.all(request);
 
-    return classificacoes;
+  return classificacoes;
 }
 
-// TODO: consertar isso aqui
 export async function read(classificacao) {
-    const db = await database.connect();
+  const db = await database.connect();
 
-    const request = `select d.nome, c.nome, s.nome 
-                    from classificacao cf, departamento d, categoria c, subcategoria s 
-                    on d.cod_dep = cf.cod_dep and c.cod_cat = cf.cod_cat
-                    and s.cod_sub = cf.cod_sub;
-                    `;
+  const request = `select d.nome as dep, c.nome as cat, s.nome as sub
+                    from classificacao cf
+                    join departamento d on cf.cod_dep = d.cod_dep
+                    join categoria c on cf.cod_cat = c.cod_cat
+                    join subcategoria s on cf.cod_sub = s.cod_sub
+                    where cf.cod_dep = ? and
+                    cf.cod_cat = ? and
+                    cf.cod_sub = ?;`;
 
-    const classificacoes = await db.all(request);
+  const { dep, cat, sub } = await db.get(request, classificacao);
 
-    return classificacoes;
+  return [dep, cat, sub];
 }
-
-console.log(await read([1, 2, 3]))
 
 // TODO: Criar as classificações para criar as peças
 
-export default create;
+export default { create, read, readAll };
--- server/src/modulos/user.js
+++ server/src/modulos/user.js
@@ -1,96 +1,40 @@
 import database from '../database/database.js';
 
-const users = [
-    {
-      "cod_usr": "0b46bc21-4e5a-4fd7-a542-6d4e01d85c55",
-      "email": "joao.silva@example.com",
-      "senha": "aBcD1234",
-      "nome": "João da Silva",
-      "telefone": 1234567890,
-      "UF": "SP",
-      "cidade": "São Paulo",
-      "rua": "Rua dos Exemplos",
-      "número": 123
-    },
-    {
-      "cod_usr": "06b48778-0e6b-45be-bb2c-d9e9f21b485f",
-      "email": "maria.souza@example.com",
-      "senha": "eFgH5678",
-      "nome": "Maria Souza",
-      "telefone": 9876543210,
-      "UF": "RJ",
-      "cidade": "Rio de Janeiro",
-      "rua": "Avenida das Amostras",
-      "número": 456
-    },
-    {
-      "cod_usr": "f4e43a39-6b2f-4682-96d9-aa9d6be3d66a",
-      "email": "carlos.rodrigues@example.com",
-      "senha": "iJkL9012",
-      "nome": "Carlos Rodrigues",
-      "telefone": 5555555555,
-      "UF": "MG",
-      "cidade": "Belo Horizonte",
-      "rua": "Travessa dos Testes",
-      "número": 789
-    },
-    {
-      "cod_usr": "c23d5e8d-5e21-4089-b27a-8a8d23d32096",
-      "email": "ana.santos@example.com",
-      "senha": "mNoP3456",
-      "nome": "Ana Santos",
-      "telefone": 1111111111,
-      "UF": "RS",
-      "cidade": "Porto Alegre",
-      "rua": "Rua dos Resultados",
-      "número": 1011
-    },
-    {
-      "cod_usr": "22a2a83c-42eb-41f7-9e5c-ba314f5e4d88",
-      "email": "pedro.almeida@example.com",
-      "senha": "qRsT7890",
-      "nome": "Pedro Almeida",
-      "telefone": 9999999999,
-      "UF": "BA",
-      "cidade": "Salvador",
-      "rua": "Avenida dos Exemplos",
-      "número": 1213
-    }
-]
-
-for (const user of users) {
-    const last_id = await create(user);
-
-    console.log(last_id);
-
-    const usuarioCriado = await read(user.cod_usr);
-
-    console.log(usuarioCriado);
-}
-
-async function create(user) {
+export async function create(user) {
     const db = await database.connect();
 
-    const { cod_usr, email, senha, nome, telefone, UF:uf, cidade, rua, 'número':numero } = user;
+    const { cod_usr, email, senha, nome, telefone, 
+    UF:uf, cidade, rua, 'número':numero } = user;
 
     const request = `
     insert into usuario values 
     (?, ?, ?, ?, ?, ?, ?, ?, ?);
     `;
 
-    const { lastID } = await db.run(request, [cod_usr, email, senha, nome, telefone, uf, cidade, rua, numero]);
+    const { lastID } = await db.run(request, [cod_usr, 
+    email, senha, nome, telefone, uf, cidade, rua, numero]);
 
     return lastID;
 }
 
-async function read(id) {
-    const db = await Database.connect();
+export async function read(id) {
+    const db = await database.connect();
 
     const request = `select * from usuario where cod_usr = ?`;
 
     const usuario = await db.get(request, [id]);
 
     return usuario;
+}
+
+export async function readAll() {
+    const db = await database.connect();
+
+    const request = `select * from classificacao;`;
+
+    const usuarios = await db.all(request);
+
+    return usuarios;
 }
 
 export default { read, create };
