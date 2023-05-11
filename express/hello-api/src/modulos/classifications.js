fac211a411497c91c658989df815a54339c43d7e
slordian <92448606+slordian@users.noreply.github.com>
2023-05-11 14:43:42 +0000

    conserta isso ai

--- express/hello-api/src/modulos/classifications.js
+++ express/hello-api/src/modulos/classifications.js
@@ -10,7 +10,33 @@ export async function create(classif) {
     return lastID;
 }
 
-// TODO: Criar as funções de leitura
+export async function readAll() {
+    const db = await database.connect();
+
+    const request = `select * from classificacao;`;
+
+    const classificacoes = await db.all(request);
+
+    return classificacoes;
+}
+
+// TODO: consertar isso aqui
+export async function read(classificacao) {
+    const db = await database.connect();
+
+    const request = `select d.nome, c.nome, s.nome 
+                    from classificacao cf, departamento d, categoria c, subcategoria s 
+                    on d.cod_dep = cf.cod_dep and c.cod_cat = cf.cod_cat
+                    and s.cod_sub = cf.cod_sub;
+                    `;
+
+    const classificacoes = await db.all(request);
+
+    return classificacoes;
+}
+
+console.log(await read([1, 2, 3]))
+
 // TODO: Criar as classificações para criar as peças
 
 export default create;
