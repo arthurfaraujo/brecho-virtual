bd2db14b902c99484a690321fabf55b4240b7cdc
slordian <arthurfelipe5567@gmail.com>
2023-06-15 21:15:12 -0300

    feat: change source of data
    now the source is the prisma database

--- api/src/routes/Data.js
+++ api/src/routes/Data.js
@@ -1,14 +1,13 @@
 import { Router } from 'express'
-import Clothes from '../database/models/clothes.js'
-
+import productModel from '../models/productModel.js'
 const rota = Router()
 
 // acesso do front à parte de dados
 // envia os produtos
 rota.get('/produtos', async (req, res, next) => {
   try {
-    // console.log(await Clothes.readAll());
-    res.json(await Clothes.readAll())
+    // console.log(await productModel.readAll())
+    res.json(await productModel.readAll())
   } catch (e) {
     next(e)
   }
