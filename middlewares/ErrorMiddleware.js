7e329e30d2c4bf96943bc7072e9857e2f9f8c1a8
slordian <92448606+slordian@users.noreply.github.com>
2023-07-07 11:12:42 +0000

    fix: add a secondary error message

--- middlewares/ErrorMiddleware.js
+++ middlewares/ErrorMiddleware.js
@@ -7,9 +7,9 @@ function otherErrors (err, req, res, next) {
   if (err.code) {
     if (parseInt(err.code) < 600) {
       // res.status(err.code).json({ message: err.message })
-      res.status(err.code).render('error', { errorMessage: err.messageUsr, errorCode: err.code })
+      res.status(err.code).render('error', { errorMessage: err.messageUsr || err.message, errorCode: err.code })
     } else {
-      res.status(400).render('error', { errorMessage: err.messageUsr, errorCode: 400 })
+      res.status(400).render('error', { errorMessage: err.messageUsr || err.message, errorCode: 400 })
     }
   } else {
     // res.status(500).json({ message: 'Algo deu muito errado!' })
